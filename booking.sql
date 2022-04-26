cab7bf27f5007a0213c6cf3ab8e164cc8e8f21dc
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-26 19:15:11 -0400

    c

--- booking.sql
+++ booking.sql
@@ -3,13 +3,11 @@ USE `booking`;
 
 CREATE TABLE `users` (
   `id` int(11) AUTO_INCREMENT,
-  `email` varchar(255),
-  `firstName` varchar(255),
-  `lastName` varchar(255),
+  `username` varchar(255),
   `password` varchar(255),
   PRIMARY KEY(`id`)
 );
 
-INSERT INTO `users` (`id`, `email`, `firstName`, `lastName`, `password`) VALUES
-(1, 'nora.hills@gmail.com', 'nora', 'hills', '$2y$10$jhSIk2N5BnkEEzgEBWQDw.AUQIEcrH8V0AcNLfW2nkjTAH2WgAAlW');
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'nora.hills', '$2y$10$jhSIk2N5BnkEEzgEBWQDw.AUQIEcrH8V0AcNLfW2nkjTAH2WgAAlW');
 

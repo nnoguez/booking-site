b31e1c408c4a611b4b5f1f71a74a6e41280b7cd1
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-28 16:42:15 -0400

    c

--- booking.sql
+++ booking.sql
@@ -4,10 +4,10 @@ USE `booking`;
 CREATE TABLE `users` (
   `id` int(11) AUTO_INCREMENT,
   `username` varchar(255),
-  -- `firstName` varchar(255),
+  `firstName` varchar(255),
   `password` varchar(255),
   PRIMARY KEY(`id`)
 );
 
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'username', '$2y$10$jhSIk2N5BnkEEzgEBWQDw.AUQIEcrH8V0AcNLfW2nkjTAH2WgAAlW');
+INSERT INTO `users` (`id`, `username`, `firstName`, `password`) VALUES
+(1, 'username', 'firstName', '$2y$10$jhSIk2N5BnkEEzgEBWQDw.AUQIEcrH8V0AcNLfW2nkjTAH2WgAAlW');

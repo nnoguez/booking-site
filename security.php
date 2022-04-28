179e6a0e4620f68ce22c4e9752f8b33b9726a3c0
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-27 20:33:42 -0400

    commit

--- security.php
+++ security.php
@@ -67,49 +67,49 @@
         setcookie("login", "yes", time() - 10);
     }
 
-    function security_sanitize() {
-        // Create an array of keys username and password
-        $result = [
-            "username" => null,
-            "password" => null,
-            "newPassword" => null
-        ];
+    // function security_sanitize() {
+    //     // Create an array of keys username and password
+    //     $result = [
+    //         "username" => null,
+    //         "password" => null,
+    //         "newPassword" => null
+    //     ];
 
-        if(security_validate()) {
-            // After validation, sanitize text input.
-            $result["username"] = htmlspecialchars($_POST["username"]);
-            $result["password"] = htmlspecialchars($_POST["password"]);
-            $result["newPassword"] = htmlspecialchars($_POST["newPassword"]);
-        }
+    //     if(security_validate()) {
+    //         // After validation, sanitize text input.
+    //         $result["username"] = htmlspecialchars($_POST["username"]);
+    //         $result["password"] = htmlspecialchars($_POST["password"]);
+    //         $result["newPassword"] = htmlspecialchars($_POST["newPassword"]);
+    //     }
 
-        // Return array
-        return $result;
-    }
+    //     // Return array
+    //     return $result;
+    // }
 
-    // starting lab 10 changes 
-    function security_deleteUser() {
-        // Validate and sanitize.
-        $result = security_sanitize();
-        // Open connection.
-        database_connect();
-        // Use connection.
-            database_deleteUser($result["username"], $result["password"]);
-        // Close connection.
-        database_close();
-    }
+    // // starting lab 10 changes 
+    // function security_deleteUser() {
+    //     // Validate and sanitize.
+    //     $result = security_sanitize();
+    //     // Open connection.
+    //     database_connect();
+    //     // Use connection.
+    //         database_deleteUser($result["username"], $result["password"]);
+    //     // Close connection.
+    //     database_close();
+    // }
 
-    function security_updatePassword() {
-        // Validate and sanitize.
-        $result = security_sanitize();
-        // $newPassword = $_POST['newPassword'];
-        // Open connection.
-        database_connect();
-        // Use connection.
-            // Username does not exist.
-            // Add a new one.
-            database_updatePassword($result["username"],  $result["password"], $result["newPassword"]);
-        // Close connection.
-        database_close();
-    }
+    // function security_updatePassword() {
+    //     // Validate and sanitize.
+    //     $result = security_sanitize();
+    //     // $newPassword = $_POST['newPassword'];
+    //     // Open connection.
+    //     database_connect();
+    //     // Use connection.
+    //         // Username does not exist.
+    //         // Add a new one.
+    //         database_updatePassword($result["username"],  $result["password"], $result["newPassword"]);
+    //     // Close connection.
+    //     database_close();
+    // }
 ?>
 

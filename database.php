46bd20e8968668d175393d9f6939123fd9f91e03
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-28 18:30:43 -0400

    c

--- database.php
+++ database.php
@@ -10,6 +10,8 @@
         $server = "localhost";
         // Username
         $firstName = "root";
+        $username = "root";
+
         // If using XAMPP, 
         //  the password is an empty string.
         $password = "root";
@@ -17,11 +19,11 @@
         $database = "booking";
 
         if($connection == null) {
-            $connection = mysqli_connect($server, $firstName, $password, $database);
+            $connection = mysqli_connect($server, $firstName, $username, $password, $database);
         }
     }
 
-    function database_addUser($firstName, $password) {
+    function database_addUser($firstName, $username, $password) {
         // Use the global connection
         global $connection;
 
@@ -29,11 +31,11 @@
             // Overwrite the existing password value as a hash
             $password = password_hash($password, PASSWORD_DEFAULT);
             // Insert username and hashed password
-            mysqli_query($connection, "INSERT INTO users (firstName, password) VALUES ('{$firstName}', '{$password}');");
+            mysqli_query($connection, "INSERT INTO users (firstName, username, password) VALUES ('{$firstName}', '{$username}', '{$password}');");
         }
     }
 
-    function database_verifyUser($firstName, $password) {
+    function database_verifyUser($firstName, $username, $password) {
         // Use the global connection
         global $connection;
 

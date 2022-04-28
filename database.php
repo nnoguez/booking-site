7faa4334a50a131ba647b9ad9946fb1a71501c5e
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-28 18:22:38 -0400

    c

--- database.php
+++ database.php
@@ -9,7 +9,7 @@
         // Server
         $server = "localhost";
         // Username
-        $username = "root";
+        $firstName = "root";
         // If using XAMPP, 
         //  the password is an empty string.
         $password = "root";
@@ -17,11 +17,11 @@
         $database = "booking";
 
         if($connection == null) {
-            $connection = mysqli_connect($server, $username, $password, $database);
+            $connection = mysqli_connect($server, $firstName, $password, $database);
         }
     }
 
-    function database_addUser($username, $password) {
+    function database_addUser($firstName, $password) {
         // Use the global connection
         global $connection;
 
@@ -29,11 +29,11 @@
             // Overwrite the existing password value as a hash
             $password = password_hash($password, PASSWORD_DEFAULT);
             // Insert username and hashed password
-            mysqli_query($connection, "INSERT INTO users (username, password) VALUES ('{$username}', '{$password}');");
+            mysqli_query($connection, "INSERT INTO users (firstName, password) VALUES ('{$firstName}', '{$password}');");
         }
     }
 
-    function database_verifyUser($username, $password) {
+    function database_verifyUser($firstName, $password) {
         // Use the global connection
         global $connection;
 
@@ -42,7 +42,7 @@
 
         if($connection != null) {
             // Use WHERE expressions to look for username
-            $results = mysqli_query($connection, "SELECT password FROM users WHERE username = '{$username}';");
+            $results = mysqli_query($connection, "SELECT password FROM users WHERE firstName = '{$firstName}';");
             
             // mysqli_fetch_assoc() returns either null or row data
             $row = mysqli_fetch_assoc($results);

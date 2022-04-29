fd7756c6bd8355af8aaa7097eba832f924856fab
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-28 22:41:07 -0400

    n

--- database.php
+++ database.php
@@ -31,7 +31,7 @@
             // Overwrite the existing password value as a hash
             $password = password_hash($password, PASSWORD_DEFAULT);
             // Insert username and hashed password
-            mysqli_query($connection, "INSERT INTO users (username, firstName, password) VALUES ('{$username}', '{$firstName}', '{$password}');");
+            mysqli_query($connection, "INSERT INTO users (username, password) VALUES ('{$username}', '{$password}');");
         }
     }
 

4ed5078b27d76bc58268ef58b39f704223dce15f
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-29 10:52:33 -0400

    n

---
+++ ticket.php
@@ -0,0 +1,19 @@
+<?php
+    include("security.php");
+?>
+<html>
+    <head>
+        <title> Portfolio 2 </title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div class="header">
+            <h1> MCO e-Ticket Booking ✈ </h1>
+        </div>
+        <div class="box">
+            <?php
+
+            ?>
+        </div>
+    </body>
+</html>

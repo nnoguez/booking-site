4b9a9997b07e2874f5e2ccf22bcf6961acff0cb8
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-29 00:07:57 -0400

    b

--- login.php
+++ login.php
@@ -22,13 +22,15 @@
                 <a class='back' href='index.php'><h3>⬅︎</h3></a>
                 <h2> Log In </h2>
                 <form method="POST">
-                    <input type="username" name="username" placeholder="Username"/>
-                    <!-- <br> -->
-                    <!-- <input type="firstName" name="firstName" placeholder="First Name"/> -->
+                <input type="text" name="username" placeholder="Username"/>
+                    <br>
+                    <input type="text" name="firstName" placeholder="First Name"/> 
+                    <br>
+                    <input type="lastName" name="lastName" placeholder="Last Name"/>
                     <br>
                     <input type="password" name="password" placeholder="Password"/>
                     <br>
-                    <input class="button" type="submit" name="submit" value="Log in"/>
+                    <input class="button" type="submit" name="submit" value="Log In"/>
                 </form>
             <?php
                     if(isset($_POST['submit'])) {

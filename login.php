1148fc6f844f54d8463b4c4c7fcc0ed8191a2dc4
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-29 01:26:31 -0400

    swap

--- login.php
+++ login.php
@@ -3,43 +3,31 @@
 ?>
 <html>
     <head>
-        <title> Portfolio 2 </title>
+        <title> Lab 9 </title>
         <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-        <div class="header">
-            <h1> MCO e-Ticket Booking ✈ </h1>
-        </div>
-        <div class="box">
-            <?php  
-                if(security_loggedIn()) { 
-                    echo("You are already logged in.");  
-                    echo("<a href='index.php'>Return Home</a><br>"); 
-            ?>
-            <?php
-            } else {
-            ?>
-                <a class='back' href='index.php'><h3>⬅︎</h3></a>
-                <h2> Log In </h2>
-                <form method="POST">
-                <input type="text" name="username" placeholder="Username"/>
-                    <br>
-                    <input type="text" name="firstName" placeholder="First Name"/> 
-                    <br>
-                    <input type="lastName" name="lastName" placeholder="Last Name"/>
-                    <br>
-                    <input type="password" name="password" placeholder="Password"/>
-                    <br>
-                    <input class="button" type="submit" name="submit" value="Log In"/>
-                </form>
-            <?php
-                    if(isset($_POST['submit'])) {
-                        security_login();
-                        security_loggedIn();
-                        echo ("Logged In");
-                    }
+        <h1> Log In </h1>
+        <?php  
+            if(security_loggedIn()) { 
+                echo("You are already logged in.");  
+                echo("<a href='index.php'>Return Home</a><br>"); 
+        ?>
+        <?php
+           } else {
+        ?>
+            <form method="POST">
+                <input type="username" name="username" placeholder="Enter a Username"/>
+                <input type="password" name="password" placeholder="Enter a Password"/>
+                <input type="submit" name="submit" value="submit"/>
+            </form>
+        <?php
+                if(isset($_POST['submit'])) {
+                    security_login();
+                    security_loggedIn();
+                    echo ("Logged In");
                 }
-            ?>
-        </div>
+            }
+        ?>
     </body>
 </html>

10bbb689d87b0fb7f8ca806563e9db80e851becd
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-29 02:00:08 -0400

    c

--- index.php
+++ index.php
@@ -12,35 +12,33 @@
         </div>
         <div class="box">
             <?php
-                // if(security_loggedIn()) { 
-                //     echo("Enter <br>"); 
-                //     // echo("<a href='logout.php'>Log Out</a>");  
-                // } else {
-                //     echo("
-                //         <div>
-                //             <h2>It seems as if you're not logged in. Would you like to:</h2>
-                //             <br>
-                //             <div class='box-item'>
-                //                 <a href='signup.php'><button class='button'>Sign Up</button></a>
-                //                 <br>
-                //                 <a href='login.php'><button class='button'>Login</button></a>
-                //             </div>
-                //         </div>
-                //     ");  
-                // }
-
-
-
                 if(security_loggedIn()) { 
-                    echo("It seems as if you're logged in. Would you like to: <br>");
-                    echo("<a href='update.php'>Update Password</a><br>"); 
-                    echo("<a href='remove.php'>Delete Account</a><br>");  
-                     // adding this in to redirect user back to login page, thought it would help with navigation
-                    echo("<a href='logout.php'>Log Out</a>");  
+                    echo("
+                        <div>
+                            <h2> It seems as if you're logged in. Would you like to: </h2>
+                            <br>
+                            <div class='box-item'>
+                                <a href='update.php'>Update Password</a>
+                                <br>
+                                <a href='remove.php'>Delete Account</a>
+                                <br>
+                                <a href='logout.php'>Log Out</a>
+                            </div>
+                        </div>
+                    ");
+
                 } else {
-                    echo("It seems as if you're not logged in. Would you like to: <br>");
-                    echo("<a href='signup.php'>Sign Up</a><br>");  
-                    echo("<a href='login.php'>Login</a>");  
+                    echo("
+                        <div>
+                            <h2>It seems as if you're not logged in. Would you like to:</h2>
+                            <br>
+                            <div class='box-item'>
+                                <a href='signup.php'><button class='button'>Sign Up</button></a>
+                                <br>
+                                <a href='login.php'><button class='button'>Login</button></a>
+                            </div>
+                        </div>
+                    ");  
                 }
             ?>
         </div>

32062fb77804948162fdc9c9da5edd6227ec7b08
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-29 12:11:42 -0400

    change

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php
+    include("security.php");
+?>
+<html>
+    <head>
+        <title> Portfolio 2 </title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <div class="header">
+            <h1> MCO e-Ticket Booking ✈ </h1>
+        </div>
+
+        <div id="header-img">
+            <a href='index.php'><button class='button'>Enter</button></a>  
+        </div>
+    </body>
+</html>

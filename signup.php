77f1e2a39512fb85eb9fcafdf5544a3ab87a8485
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-28 21:57:48 -0400

    c

--- signup.php
+++ signup.php
@@ -21,7 +21,7 @@
             ?>
                 <a class='back' href='index.php'><h3>⬅︎</h3></a>
                 <h2> Sign Up </h2>
-                <form method="POST">
+                <form method="POST" action="index.php">
                     <input type="text" name="username" placeholder="Username"/>
                     <br>
                     <input type="text" name="firstName" placeholder="First Name"/> 

66d47d302e81f3f0b0e2d6ba023e36bc72bde85e
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-28 20:46:04 -0400

    c

--- signup.php
+++ signup.php
@@ -24,7 +24,7 @@
                 <form method="POST" action="listings.php">
                     <input type="username" name="username" placeholder="Username"/>
                     <br>
-                    <!-- <input type="firstName" name="firstName" placeholder="First Name"/>  -->
+                    <input type="firstName" name="firstName" placeholder="First Name"/> 
                     <!-- <input type="lastName" name="lastName" placeholder="Last Name"/>
                     <br> -->
                     <input type="password" name="password" placeholder="Password"/>

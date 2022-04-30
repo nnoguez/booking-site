391ec8833220efa5f493df213e892982fe094012
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-30 12:42:36 -0400

    c

--- ticket.php
+++ ticket.php
@@ -75,7 +75,7 @@
             <li style="float:right"><a class="nav-hover" href="logout.php">Log Out</a></li>
             <li style="float:right"><a class="nav-hover" href="profile.php">Profile</a></li>
         </ul>
-        <div class="box3">
+          <div class="box3">
             <h2> Ticket Information </h2>
             <div class="ticket">
                     <div class="column left-head">
@@ -85,36 +85,37 @@
                     <div class="column right-head">
                         <h1>Boarding Pass</h1>
                     </div>
+
                     <div class="ticket-inner">
-                    <div class="column left">
-                        <h4>║▌║█║▌│║▌║▌█</h4>
-                        <h4>║▌║█║▌│║▌║▌█</h4>
-                        <h4>║▌║█║▌│║▌║▌█</h4>
-                        <h4>║▌║█║▌│║▌║▌█</h4>
-                        <h4>║▌║█║▌│║▌║▌█</h4>
-                    </div>
+                      <div class="column left">
+                          <h4>║▌║█║▌│║▌║▌█</h4>
+                          <h4>║▌║█║▌│║▌║▌█</h4>
+                          <h4>║▌║█║▌│║▌║▌█</h4>
+                          <h4>║▌║█║▌│║▌║▌█</h4>
+                          <h4>║▌║█║▌│║▌║▌█</h4>
+                      </div>
 
 
-                    <div class="column middle">
-                        <b><p>Name:</p></b>
-                            <p>{$firstName, $lastName}</p>
-                        <b><p>From:</p></b>
-                            <p>MCO</p>
-                        <b><p>To:</p></b>
-                            <p>{$destination}</p>
-                    </div>
-         
-                    <div class="column right">
-                        <b><p>Name:</b> {$firstName, $lastName}</p>
-                        <b><p>From:</b> MCO</p>
-                        <b><p>To:</b> {$destination}</p>
-                        <b><p>Date:</b> 30APR <b>Time:</b> 10:45 </p>
-                        <b><p>Flight:</b> A23 <b>Gate:</b> 17 <b>Seat:</b> 23D</p>
-                        <h3> Economy </h3>
+                      <div class="column middle">
+                          <b><p>Name:</p></b>
+                              <p>{$firstName, $lastName}</p>
+                          <b><p>From:</p></b>
+                              <p>MCO</p>
+                          <b><p>To:</p></b>
+                              <p>{$destination}</p>
+                      </div>
+          
+                      <div class="column right">
+                          <b><p>Name:</b> {$firstName, $lastName}</p>
+                          <b><p>From:</b> MCO</p>
+                          <b><p>To:</b> {$destination}</p>
+                          <b><p>Date:</b> 30APR <b>Time:</b> 10:45 </p>
+                          <b><p>Flight:</b> A23 <b>Gate:</b> 17 <b>Seat:</b> 23D</p>
+                          <h3> Economy </h3>
+                      </div>  
                     </div>
                                
-                </div> 
-            </div>
-        </div>
+              </div> 
+          </div>
     </body>
 </html>

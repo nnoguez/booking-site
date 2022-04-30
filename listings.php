58911717a6663c947194f7996e17357f347764e5
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-29 22:35:58 -0400

    c

--- listings.php
+++ listings.php
@@ -22,21 +22,17 @@
                     <h2>Please select a destination:</h2>
                     <input type="radio" id="paris" name="destinations" value="paris">
                       <label for="paris">Paris, France</label><br>
-                        
                     <input type="radio" id="santiago" name="destinations" value="santiago">
                       <label for="santiago">Santiago, Chile</label><br>
-                    
                     <input type="radio" id="seoul" name="destinations" value="seoul">
                       <label for="seoul">Seoul, South Korea</label><br>
-                    
                     <input type="radio" id="tokyo" name="destinations" value="tokyo">
                       <label for="tokyo">Tokyo, Japan</label><br>
-                        
                     <input type="radio" id="sydney" name="destinations" value="sydney">
                       <label for="sydney">Sydney, Australia</label><br>
-                    
                     <input type="radio" id="toronto" name="destinations" value="toronto">
                       <label for="toronto">Toronto, Canada</label><br>
+                    <input class="button" type="submit" name="submit" value="Book Flight/>
                 </form>
             
             <?php

d0db6c879947313077da052757ce5455900b50fb
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-30 12:54:15 -0400

    c

--- ticket.php
+++ ticket.php
@@ -81,7 +81,7 @@
             <li style="float:right"><a class="nav-hover" href="logout.php">Log Out</a></li>
             <li style="float:right"><a class="nav-hover" href="profile.php">Profile</a></li>
         </ul>
-          <div class="box3">
+          <div class="box2">
             <h2> Ticket Information </h2>
             <div class="ticket">
                     <div class="column left-head">

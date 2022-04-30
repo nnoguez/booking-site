3417625aac3d0349f7a66db2b255a842ada7ae74
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-30 05:18:33 -0400

    c

--- ticket.php
+++ ticket.php
@@ -1,31 +1,35 @@
-<!DOCTYPE html>
+<?php
+    include("security.php");
+?>
 <html>
-<head>
-<style>
+    <head>
+        <title> Portfolio 2 </title>
+        <!-- <link rel="stylesheet" href="styles.css"> -->
+        <style>
   * {
       box-sizing: border-box;
     }
 
     .column {
       float: left;
-      padding: 5px;
+      /* padding: 5px; */
     }
 
     .left {
-      width: 20%;
-      text-align: left;
+      width: 10%;
       background-color: white;
+      text-align: left;
     }
 
 	.middle {
-      width: 40%;
-      text-align: left;
+      width: 30%;
       background-color: white;
+      text-align: left;
     }
     
     .right {
-      width: 40%;
-      text-align: left;      
+      width: 30%;
+      text-align: left;
       background-color: white;
     }
     
@@ -55,53 +59,58 @@
   
   .ticket-inner {
   	background-color: white;
-
     margin: 0px;
     padding: 0px;
   }
 </style>
-</head>
-<body>
-<div class="row">
-<div class="ticket">
-	<div class="column left-head">
-		<h1> Sky Airlines ✈</h1>
-    </div>
-    
-    <div class="column right-head">
-		<h1>Boarding Pass</h1>
-    </div>
-    
-	<div class="column left">
-    	<h4>║█║▌║</h4>
-    	<h4>║█║▌║</h4>
-    	<h4>║█║▌║</h4>
-    	<h4>║█║▌║</h4>
-    	<h4>║█║▌║</h4>
-    </div>
-    
-    <div class="column middle">
-    	<b><p>Name:</p></b>
-        	<p>{$firstName, $lastName}</p>
-		<b><p>From:</p></b>
-			<p>MCO</p>
-		<b><p>To:</p></b>
-			<p>{$destination}</p>
-    </div>
-    
-	<div class="column right">
-		<b><p>Name:</b> {$firstName, $lastName}</p>
-		<b><p>From:</b> MCO</p>
-		<b><p>To:</b> {$destination}</p>
-		<b><p>Date:</b> 30APR <b>Time:</b> 10:45 </p>
-        <b><p>Flight:</b> A23 <b>Gate:</b> 17 <b>Seat:</b> 23D</p>
-        <h3> Economy </h3>
-</div>
+    </head>
+    <body>
+        <ul>
+            <li><a href="index.php">MCO e-Ticket Booking ✈</a></li>
+            <li><a class="nav-hover" href="home.php">Home</a></li>
+            <li><a class="nav-hover" href="listings.php">Listings</a></li>
+            <li style="float:right"><a class="nav-hover" href="logout.php">Log Out</a></li>
+            <li style="float:right"><a class="nav-hover" href="profile.php">Profile</a></li>
+        </ul>
+        <div class="box3">
+            <h2> Ticket Information </h2>
+            <div class="ticket">
+                <div class="ticket-inner">
+                    <div class="column left-head">
+                        <h1> Sky Airlines ✈</h1>
+                    </div>
+                    
+                    <div class="column right-head">
+                        <h1>Boarding Pass</h1>
+                    </div>
+              
+                    <div class="column left">
+                        <h4>║█║▌║</h4>
+                        <h4>║█║▌║</h4>
+                        <h4>║█║▌║</h4>
+                        <h4>║█║▌║</h4>
+                        <h4>║█║▌║</h4>
+                    </div>
 
- 
-
-</div>
-</div>
-</body>
+                    <div class="column middle">
+                        <b><p>Name:</p></b>
+                            <p>{$firstName, $lastName}</p>
+                        <b><p>From:</p></b>
+                            <p>MCO</p>
+                        <b><p>To:</p></b>
+                            <p>{$destination}</p>
+                    </div>
+                    
+                    <div class="column right">
+                        <b><p>Name:</b> {$firstName, $lastName}</p>
+                        <b><p>From:</b> MCO</p>
+                        <b><p>To:</b> {$destination}</p>
+                        <b><p>Date:</b> 30APR <b>Time:</b> 10:45 </p>
+                        <b><p>Flight:</b> A23 <b>Gate:</b> 17 <b>Seat:</b> 23D</p>
+                        <h3> Economy </h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
-

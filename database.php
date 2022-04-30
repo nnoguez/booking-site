889f710f199dba9289450e88b2d3c9b7332f67d7
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-30 03:12:17 -0400

    c

--- database.php
+++ database.php
@@ -72,7 +72,7 @@
         }
     }
 
-    function database_addDestination() {
+    function database_addDestination($destinationReplaced) {
         // Use the global connection
         global $connection;
 

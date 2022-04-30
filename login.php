22bdf23a9690aa9a70328dfc92ca747fa11e2ae2
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-30 15:39:24 -0400

    c

--- login.php
+++ login.php
@@ -7,22 +7,30 @@
         <link rel="stylesheet" href="styles.css">
     </head>
     <body>
-        <div class="header">
-            <h1> MCO e-Ticket Booking ✈ </h1>
-        </div>
+        <ul>
+            <li><a href="index.php">MCO e-Ticket Booking ✈</a></li>
+            <li><a class="nav-hover" href="home.php">Home</a></li>
+            <li style="float:right"><a class="nav-hover" href="signup.php">Sign Up</a></li>
+            <li class="active" style="float:right"><a class="nav-hover" href="login.php">Log In</a></li>
+        </ul>
         <div class="box">
             <?php  
                 if(security_loggedIn()) { 
-                    echo("You are already logged in.");  
-                    echo("<a href='index.php'>Return Home</a><br>"); 
+                    echo("
+                    <a class='a-par' href='listings.php'><button class='button'> Go to Site </button></a>
+                    <br>
+                    <a class='a-par' href='home.php'>Return Home</a>
+                    <br>
+                    <a class='a-par' href='logout.php'>Log Out</a>
+                    "); 
             ?>
             <?php
             } else {
             ?>
-                <a class='back' href='index.php'><h3>⬅︎</h3></a>
+                <a class='back' href='home.php'><h3>⬅︎</h3></a>
                 <h2> Log In </h2>
                 <form method="POST">
-                <input type="text" name="username" placeholder="Username"/>
+                    <input type="text" name="username" placeholder="Username"/>
                     <br>
                     <input type="text" name="firstName" placeholder="First Name"/> 
                     <br>
@@ -33,10 +41,11 @@
                     <input class="button" type="submit" name="submit" value="Log In"/>
                 </form>
             <?php
-                    if(isset($_POST['submit'])) {
-                        security_login();
-                        security_loggedIn();
-                        echo ("Logged In");
+                if(isset($_POST['submit'])) {
+                    security_login();
+                    security_loggedIn();
+                    echo ("Logged In<br>");
+                    ticketName();
                     }
                 }
             ?>

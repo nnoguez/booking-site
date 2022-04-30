cda399fdd656faa52795939e6ee0c46c2971f3f0
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-30 00:42:43 -0400

    c

--- listings.php
+++ listings.php
@@ -18,7 +18,7 @@
             <?php  
                 if(security_loggedIn()) { 
             ?>
-                <form action="/action_page.php">
+                <form>
                     <h2>Please select a destination:</h2>
                     <input type="radio" id="paris" name="destinations" value="paris">
                       <label for="paris">Paris, France</label><br>

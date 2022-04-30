6a251a89a25a7bac2d909e55c12e6d36c8eae1e4
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-30 03:13:25 -0400

    c

--- database.php
+++ database.php
@@ -73,14 +73,7 @@
     }
 
     function database_addDestination($destinationReplaced) {
-        // Use the global connection
-        global $connection;
-
-        if($connection != null) {
-            // Insert username and hashed password
-            // $destinationSelected = "hi";
             $destinationReplaced = mysqli_query($connection, "UPDATE users SET destinations = 'hi' WHERE destinations IS NULL;");
-        }
     }
 ?>
 

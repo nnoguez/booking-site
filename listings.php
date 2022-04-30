3a1efd7bfe45acdb87fdef129f2e6827fbe2281f
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-30 15:34:44 -0400

    c

--- listings.php
+++ listings.php
@@ -38,7 +38,7 @@
                 </form>
                 <?php 
                     if (isset($_POST['submit'])) {
-                        $destinationSelected = $_POST['destinations'];
+                        // $destinationSelected = $_POST['destinations'];
                         $query = "UPDATE users SET destination = hi WHERE 1;";
                         $q = mysqli_query($query);
 

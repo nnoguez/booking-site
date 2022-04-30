c18525ee4e0429c41b051b7ba3b780e4ee657438
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-30 01:37:23 -0400

    c

--- radioButton.php
+++ radioButton.php
@@ -1,38 +1,39 @@
-<FORM name ="form1" method ="post" action ="radioButton.php">
-
-<Input type = 'Radio' Name ='gender' value= 'male'
-<?PHP echo $male_status; ?>
->Male
-
-<Input type = 'Radio' Name ='gender' value= 'female'
-<?PHP echo $female_status; ?>
->Female
-
-<P>
-<Input type = "Submit" Name = "Submit1" VALUE = "Select a Radio Button">
-
-</FORM>
+<html>
+<head>
+<title>Radio Buttons</title>
+</head>
 
 <?PHP
 
 $male_status = 'unchecked';
 $female_status = 'unchecked';
 
+
 if (isset($_POST['Submit1'])) {
 
-$selected_radio = $_POST['gender'];
+	$selected_radio = $_POST['gender'];
+	
+		if ($selected_radio == 'male') {
+			$male_status = 'checked';
 
-if ($selected_radio == 'male') {
-
-$male_status = 'checked';
-
-}
-else if ($selected_radio == 'female') {
-
-$female_status = 'checked';
-
-}
-
+		}
+		else if ($selected_radio == 'female') {
+			$female_status = 'checked';
+		}
 }
 
 ?>
+
+<body>
+
+<FORM NAME ="form1" METHOD ="POST" ACTION ="radioButton.php">
+
+<INPUT TYPE = 'Radio' Name ='gender'  value= 'male' <?PHP print $male_status; ?>>Male
+
+<INPUT TYPE = 'Radio' Name ='gender'  value= 'female' <?PHP print $female_status; ?>>Female
+<P>
+<INPUT TYPE = "Submit" Name = "Submit1"  VALUE = "Select a Radio Button">
+</FORM>
+
+</body>
+</html>

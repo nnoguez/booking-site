0b29af0f71d810c093f6b13ffacb60f29aeea77c
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-30 04:56:32 -0400

    c

--- ticket.php
+++ ticket.php
@@ -16,38 +16,43 @@
         </ul>
         <div class="box">
             <h2> Ticket Information </h2>
-            <?php
-                // if($connection != null) {
-                //     // Get the results of a query using the connection
-                //     // TODO: Write SQL SELECT statement to read first name, last name, city, and state.
-                //     $query = "SELECT `first_name`, `last_name`, `city`, `state` FROM `customers`;";
-                //     $results = mysqli_query($connection, $query);
-        
+            <div class="ticket">
+                <div class="ticket-inner">
+                    <div class="column left-head">
+                        <h1> Sky Airlines ✈</h1>
+                    </div>
                     
-                //     // Start the HTML table.
-                //     echo("<table><tbody>");
-        
-                //     // For every row, generate a new HTML row.
-                //     while($row = mysqli_fetch_assoc($results)) {
-                //         // Start the row.
-                //         echo("<tr>");
-        
-                //         // TODO: for each key, add a column entry in HTML using echo().
-                //         echo("<td>".$row["first_name"]."</td>");
-                //         echo("<td>".$row["last_name"]."</td>");
-                //         echo("<td>".$row["city"]."</td>");
-                //         echo("<td>".$row["state"]."</td>");
-                        
-                //         // Reminder: HTML tables use <td> (https://developer.mozilla.org/en-US/docs/Web/HTML/Element/table#examples).
-                        
-                //         // End the row.
-                //         echo("</tr>");
-                //     }
-        
-                //     // End the HTML table.
-                //     echo("</tbody></table>");
-                // }
-            ?>
+                    <div class="column right-head">
+                        <h1>Boarding Pass</h1>
+                    </div>
+                    
+                    <div class="column left">
+                        <h4>║█║▌║</h4>
+                        <h4>║█║▌║</h4>
+                        <h4>║█║▌║</h4>
+                        <h4>║█║▌║</h4>
+                        <h4>║█║▌║</h4>
+                    </div>
+                    
+                    <div class="column middle">
+                        <b><p>Name:</p></b>
+                            <p>{$firstName, $lastName}</p>
+                        <b><p>From:</p></b>
+                            <p>MCO</p>
+                        <b><p>To:</p></b>
+                            <p>{$destination}</p>
+                    </div>
+                    
+                    <div class="column right">
+                        <b><p>Name:</b> {$firstName, $lastName}</p>
+                        <b><p>From:</b> MCO</p>
+                        <b><p>To:</b> {$destination}</p>
+                        <b><p>Date:</b> 30APR <b>Time:</b> 10:45 </p>
+                        <b><p>Flight:</b> A23 <b>Gate:</b> 17 <b>Seat:</b> 23D</p>
+                        <h3> Economy </h3>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 </html>

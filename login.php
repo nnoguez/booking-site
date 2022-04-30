ca51891689dae63f28d930d7571e1e0c90f930da
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-29 21:30:10 -0400

    c

--- login.php
+++ login.php
@@ -32,10 +32,6 @@
                 <form method="POST">
                     <input type="text" name="username" placeholder="Username"/>
                     <br>
-                    <input type="text" name="firstName" placeholder="First Name"/> 
-                    <br>
-                    <input type="lastName" name="lastName" placeholder="Last Name"/>
-                    <br>
                     <input type="password" name="password" placeholder="Password"/>
                     <br>
                     <input class="button" type="submit" name="submit" value="Log In"/>

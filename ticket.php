556d2a64ddd93bc149ff16ba78e7e759befb868a
naomy <92695534+nnoguez@users.noreply.github.com>
2022-04-30 22:24:23 -0400

    c

--- ticket.php
+++ ticket.php
@@ -106,7 +106,8 @@
 
                       <div class="column middle">
                           <b><p>Name:</p></b>
-                              <p><?php echo $_SESSION['firstName'], " ", $_SESSION['lastName']; ?></p>
+                              <p><?php echo $_SESSION['firstName']; 
+                              echo $_SESSION['lastName']; ?></p>
                        
                           <b><p>From:</p></b>
                               <p>MCO</p>
